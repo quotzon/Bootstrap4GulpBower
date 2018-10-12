df4ad3bd16c6d0b9546edf0e8fd98184baa1a469
Denis <quotzon@gmail.com>
2018-10-12 21:31:42 +0300

    Загрузка файлов
    Загружены основные файлы для быстрой сборки проекта

---
+++ src/assets/js/main.js
@@ -0,0 +1,5 @@
+//= ../../../bower_components/jquery/dist/jquery.js
+//= ../../../bower_components/popper.js/dist/umd/popper.js
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/tooltip.js
+//= custom/tooltip.js
